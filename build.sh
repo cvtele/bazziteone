9e4585f45984dccfd2add08bd46b96b3d933c95b
cvtele <10005819+cvtele@users.noreply.github.com>
2024-07-07 13:39:09 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,6 +15,9 @@ RELEASE="$(rpm -E %fedora)"
 # this installs a package from fedora repos
 #rpm-ostree override remove kmod-xone-6.9.6-201
 rpm-ostree override remove kmod-xone-6.9.7-201.fsync.fc40.x86_64-0.0.git.115.fdbb71f1-1.fc40.x86_64 xone-kmod-common-0.0.git.115.fdbb71f1-1.fc40.noarch
+rpm-ostree status
+rpm-ostree cleanup -bpm
+rpm-ostree status
 
 curl -Lo /etc/yum.repos.d/sentry-xone.repo https://copr.fedorainfracloud.org/coprs/sentry/xone/repo/fedora-"${RELEASE}"/sentry-xone-fedora-"${RELEASE}".repo
 rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:sentry:xone xone
