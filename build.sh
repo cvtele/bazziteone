c13d9d422b009ead83c1f3147ad1c05b933fd084
cvtele <10005819+cvtele@users.noreply.github.com>
2024-07-14 16:55:49 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,12 +16,8 @@ RELEASE="$(rpm -E %fedora)"
 #rpm-ostree override remove kmod-xone-6.9.6-201
 
 #rpm-ostree override remove kmod-xone-6.9.7-201.fsync.fc40.x86_64-0.0.git.115.fdbb71f1-1.fc40.x86_64 xone-kmod-common-0.0.git.115.fdbb71f1-1.fc40.noarch
-j=""
-for i in $(rpm -qa | grep xone)
-do
-  j="$j $i"
-done
-rpm-ostree uninstall $j
+
+rpm-ostree override remove xone-kmod-common xone-kmod wireless-regdb
 
 #rpm-ostree override remove kmod-xone  xone-kmod-common
 #rpm-ostree status
