89ce5d24cac600e541e03cf3f5f981c27e12f11d
cvtele <10005819+cvtele@users.noreply.github.com>
2024-07-15 20:03:12 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -30,8 +30,8 @@ RELEASE="$(rpm -E %fedora)"
 #rpm-ostree install akmods
 curl -Lo /etc/yum.repos.d/sentry-xone.repo https://copr.fedorainfracloud.org/coprs/sentry/xone/repo/fedora-"${RELEASE}"/sentry-xone-fedora-"${RELEASE}".repo
 curl -Lo /etc/yum.repos.d/ublue-os-akmods.repo https://copr.fedorainfracloud.org/coprs/ublue-os/akmods/repo/fedora-"${RELEASE}"/ublue-os-akmods-fedora-"${RELEASE}".repo
-#rpm-ostree override replace --experimental --freeze --from repo=copr:copr.fedorainfracloud.org:ublue-os:akmods xone-kmod-common
-rpm-ostree override replace --experimental --freeze --from repo=copr:copr.fedorainfracloud.org:sentry:xone akmod-xone
+rpm-ostree override replace --experimental --freeze --from repo=copr:copr.fedorainfracloud.org:ublue-os:akmods xone-kmod-common
+#rpm-ostree override replace --experimental --freeze --from repo=copr:copr.fedorainfracloud.org:sentry:xone akmod-xone
 
 #rpm-ostree install --experimental --from repo=copr:copr.fedorainfracloud.org:sentry:xone xone
 #rpm-ostree install xone
